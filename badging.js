551f3570dc2666ba38426c03f639053d17e85d6e
sunggook chue <cpsgchue@hotmail.com>
2020-03-29 14:39:31 -0700

    missing style for related_apps and clean badge.js file

--- badging.js
+++ badging.js
@@ -1,51 +1,8 @@
-// This code copied from https://badging-api.glitch.me/index.html.
-
-// Update the UI to indicate whether the API is supported.
-function isSupported(kind) {
-  console.log('supported', kind);
-  const divNotSupported = document.getElementById('notSupported');
-  divNotSupported.classList.toggle('hidden', true);
-  butSet.removeAttribute('disabled');
-  butClear.removeAttribute('disabled');
-  badgeVal.removeAttribute('disabled');  
-}
-
-// Wrapper to support first and second origin trial
-// See https://web.dev/badging-api/ for details.
-// function setBadge(...args) {
-//   if (navigator.setAppBadge) {
-//     navigator.setAppBadge(...args);
-//   } else if (window.ExperimentalBadge) {
-//     window.ExperimentalBadge.set(...args);
-//   }
-// }
-
-// Wrapper to support first and second origin trial
-// See https://web.dev/badging-api/ for details.
-// function clearBadge() {
-//   if (navigator.clearAppBadge) {
-//     navigator.clearAppBadge();
-//   } else if (window.ExperimentalBadge) {
-//     window.ExperimentalBadge.clear();
-//   }
-// }
 
 document.addEventListener('DOMContentLoaded', ()=> {
-  // Check if the API is supported.
-  if ('setAppBadge' in navigator) {
-    isSupported('v2')
-  }
-
-  // Check if the previous API surface is supported.
-  if ('ExperimentalBadge' in window) {
-    isSupported('v1');
-  }
-
-  const butSet = document.getElementById('butSet');
-  const butClear = document.getElementById('butClear');
   const badgeVal = document.getElementById('badgeVal');
   // Click event handler for Set button.
-  butSet.addEventListener('click', () => {
+  document.getElementById('butSet').addEventListener('click', () => {
     const val = parseInt(badgeVal.value, 10);
     if (isNaN(val)) {
       navigator.setAppBadge();
@@ -55,7 +12,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
   });
   
   // Click event handler for Clear button.
-  butClear.addEventListener('click', () => {
+  document.getElementById('butClear').addEventListener('click', () => {
     navigator.clearAppBadge();
   });
   
