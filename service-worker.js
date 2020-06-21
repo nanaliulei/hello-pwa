267cb24150df83b83469ebcf7552beae1db535d8
Sunggook Chue <sunggch@microsoft.com>
2020-06-20 17:00:38 -0700

    remove experimental feature of preloading

--- service-worker.js
+++ service-worker.js
@@ -19,12 +19,13 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     // New service worker upgrade, then delete caches other than whitelist..
-    if (self.registration.navigationPreload) {
-        await self.registration.navigationPreload.enable();
-    }
 
-    const whiteList = [precache_];  // const whiteList = [PRECACHE];
+    const whiteList = [precache_];
     event.waitUntil(
+        // if (self.registration.navigationPreload) {
+        //     await self.registration.navigationPreload.enable();
+        // }
+
         caches.keys().then(cacheNames => {
             return cacheNames.filter(cacheName => !whiteList.includes(cacheName));
         }).then(cachesToDelete => {
@@ -48,11 +49,11 @@ self.addEventListener('fetch', (event) => {
                     return cachedResponse;
                 }
 
-                const preload_response = await event.preloadResponse;
-                if (preload_response) {
-                    storeToCache(preload_response.clone());
-                    return preload_response;
-                }
+                // const preload_response = await event.preloadResponse;
+                // if (preload_response) {
+                //     storeToCache(preload_response.clone());
+                //     return preload_response;
+                // }
 
                 return fetch(event.request).then(
                     function(response) {
